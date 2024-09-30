61ac939c5b1693a17ae0c4dd7e33e5b249ed76c6
Trend20 <enockomondi305@gmail.com>
2024-10-01 01:00:19 +0300

    updating the services with ORM

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PostModule } from '../modules/post/post.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ormConfig } from '../common/config/ormConfig';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
     PostModule,
+    TypeOrmModule.forRoot(ormConfig()),
   ],
 })
 export class AppModule {}
