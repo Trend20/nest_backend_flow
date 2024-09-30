a11c9309fa3b14987a37636167035b2e83d24ee8
Trend20 <enockomondi305@gmail.com>
2024-09-30 23:41:22 +0300

    updating post controller

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { PostService } from './post.service';
+import { CreatePostDto } from './dto/create-post.dto';
+import { UpdatePostDto } from './dto/update-post.dto';
 
-@Controller('post')
-export class PostController {}
+@Controller('posts')
+export class PostController {
+  constructor(private readonly postService: PostService) {}
+
+  //   get all posts
+  @Get()
+  public async getAllPosts(): Promise<Post[]> {
+    return this.postService.getAllPosts();
+  }
+  //   get a single post
+  @Get('/:id')
+  public async getPostById(@Param('id') id: string): Promise<Post> {
+    return this.postService.getPostById(id);
+  }
+  //   create a post
+  @Post('/')
+  public async createPost(@Body() postDto: CreatePostDto): Promise<Post> {
+    return this.postService.createPost(postDto);
+  }
+  //   update a post
+  @Put('/:id')
+  public async updatePost(
+    @Param('id') id: string,
+    @Body() postDto: UpdatePostDto,
+  ): Promise<Post> {
+    return this.postService.updatePost(id, postDto);
+  }
+  //   delete a post
+  @Delete('/:id')
+  public async deletePost(@Param('id') id: string): Promise<void> {
+    await this.postService.deletePost(id);
+  }
+}
